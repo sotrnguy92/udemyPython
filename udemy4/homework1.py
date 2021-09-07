65142c53b92dec29ae6f589ecfd0be7cdef6caf2
sotrnguy92 <sonnguyen092@gmail.com>
2021-09-07 12:11:36 -0700

    adding git ignore

---
+++ udemy4/homework1.py
@@ -0,0 +1,49 @@
+###
+# Part 1
+###
+
+print('Practice', " Makes Perfect.")
+# Practice Makes Perfect.
+
+print("Children must be taught\n how to think\n NOT\n what to think")
+#Children must be taught
+# how to think
+# NOT
+# what to think
+
+print("\"\"")
+#""
+
+print('2 * 3 * 4 * 5 / 10 = ')
+#2 * 3 * 4 * 5 / 10 =
+
+print(2 * 3 * 4 * 5 / 10)
+#12.0
+
+
+###
+#Part 2
+###
+
+print('Print("Me")')
+print("Print('Me')")
+print('Print(\'Me\")')
+print("You will learn\n a LOT")
+
+###
+#Part 3
+###
+
+print("*\n* *\n* * *\n* * * *")
+
+###
+#Part 4
+###
+
+print("        *        \n      * * *      \n    * * * * *    \n  * * * * * * *  \n* * * * * * * * *")
+
+###
+#Part 5
+###
+
+print("        *        \n      * * *      \n    * * * * *    \n  * * * * * * *  \n* * * * * * * * *\n  * * * * * * *  \n    * * * * *    \n      * * *      \n        *        ")
---
+++ udemy4/test1.py
@@ -0,0 +1,10 @@
+
+#helloooo
+var100 = 100
+print(var100)
+
+
+print('son! \nhi')
+print('The best among you', "are those who have the best",  'manners and character.')
+
+
