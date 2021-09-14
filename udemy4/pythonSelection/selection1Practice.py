a5f8daaef1da705418df96fde68a7693dc1e053c
sotrnguy92 <sonnguyen092@gmail.com>
2021-09-13 18:26:55 -0700

    adding calculator function to practice

--- udemy4/pythonSelection/selection1Practice.py
+++ udemy4/pythonSelection/selection1Practice.py
@@ -1,11 +1,38 @@
 
 
-#read an integer and print if it has 1, 2, 3, 4, or 5+ digits
+# #read an integer and print if it has 1, 2, 3, 4, or 5+ digits
+def intLength():
+    number = input("enter a number: ");
 
-number = input("enter a number: ");
+    if len(number) < 5:
+        print(len(number));
+    else:
+        print("5+");
 
 
-if len(number) < 5:
-    print(len(number));
-else:
-    print("5+")
+
+
+#Create a calculator
+
+def calculator():
+    num1, operation, num2 = input("enter what you would like to calculate and include spaces: ").split();
+
+    num1 = int(num1)
+    num2 = int(num2)
+
+    if operation == "+":
+        print(num1, " + ",num2, " = ", num1+num2 );
+    elif operation == "-":
+        print(num1, " - ",num2, " = ", num1-num2 );
+    elif operation == "*":
+        print(num1, " * ", num2, " = ", num1 * num2);
+    else:
+        if num2 == 0:
+            print('undefined');
+        else:
+            print(num1, " / ", num2, " = ", num1 / num2);
+
+
+
+
+# create a function that reads
